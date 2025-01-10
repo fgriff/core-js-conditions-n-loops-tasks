994b1b69a7ea0f632b6cf5ba35cfc40ae51b420b
fgriff <anglirum@gmail.com>
2025-01-11 00:34:44 +0300

    feat: solve isContainNumber task

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -192,8 +192,17 @@ function getIndexOf(str, letter) {
  *  12345, 0    => false
  *  12345, 6    => false
  */
-function isContainNumber(/* num, digit */) {
-  throw new Error('Not implemented');
+function isContainNumber(num, digit) {
+  const transformedNum = String(num);
+  const transformedDigit = String(digit);
+
+  for (let i = 0; i < transformedNum.length; i += 1) {
+    if (transformedNum[i] === transformedDigit) {
+      return true;
+    }
+  }
+
+  return false;
 }
 
 /**
