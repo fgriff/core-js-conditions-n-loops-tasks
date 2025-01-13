55c5536e89026051f702ed99847b059dfcd9e819
fgriff <anglirum@gmail.com>
2025-01-13 16:32:54 +0300

    feat: solve rotateMatrix task

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -362,8 +362,25 @@ function getSpiralMatrix(/* size */) {
  *    [7, 8, 9]         [9, 6, 3]
  *  ]                 ]
  */
-function rotateMatrix(/* matrix */) {
-  throw new Error('Not implemented');
+function rotateMatrix(matrix) {
+  const copyMatrix = matrix;
+  const localCopyMatrix = [];
+
+  for (let i = 0; i < matrix.length; i += 1) {
+    localCopyMatrix[i] = [];
+
+    for (let j = 0; j < matrix[i].length; j += 1) {
+      localCopyMatrix[i][j] = matrix[i][j];
+    }
+  }
+
+  for (let i = 0; i < matrix.length; i += 1) {
+    for (let j = 0; j < matrix[i].length; j += 1) {
+      copyMatrix[j][matrix.length - i - 1] = localCopyMatrix[i][j];
+    }
+  }
+
+  return copyMatrix;
 }
 
 /**
