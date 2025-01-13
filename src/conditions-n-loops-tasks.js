2355e11473fedfdb84b29d9d4fd5175e3458f0c2
fgriff <anglirum@gmail.com>
2025-01-13 19:44:19 +0300

    feat: solve shuffleChar task

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -468,8 +468,29 @@ function sortByAsc(arr) {
  *  '012345', 3 => '024135' => '043215' => '031425'
  *  'qwerty', 3 => 'qetwry' => 'qtrewy' => 'qrwtey'
  */
-function shuffleChar(/* str, iterations */) {
-  throw new Error('Not implemented');
+function shuffleChar(str, iterations) {
+  let localCopyStr = str;
+
+  for (let i = 1; i <= iterations; i += 1) {
+    let even = '';
+    let odd = '';
+
+    for (let j = 0; j < str.length; j += 1) {
+      if (j % 2 === 0) {
+        even += localCopyStr[j];
+      } else {
+        odd += localCopyStr[j];
+      }
+    }
+
+    localCopyStr = even + odd;
+
+    if (localCopyStr === str) {
+      return shuffleChar(localCopyStr, iterations % i);
+    }
+  }
+
+  return localCopyStr;
 }
 
 /**
